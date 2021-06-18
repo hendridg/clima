e617c8324f88d6fc39fdf606f983b3986934fd35
hendridg <73659934+hendridg@users.noreply.github.com>
2021-06-17 20:02:32 -0400

    Testing components
    Testing to input form

--- src/App.test.js
+++ src/App.test.js
@@ -19,3 +19,26 @@ test("renders error message", () => {
   const linkElement = screen.getByText("Page Not Found")
   expect(linkElement).toBeInTheDocument()
 })
+test("test de entrada en el input Location", async () => {
+  render(<App />)
+  const input = screen.getByPlaceholderText("Location")
+  fireEvent.change(input, { target: { location: { value: "Maracay" } } })
+  expect(input.location.value).toBe("Maracay")
+})
+
+it('calls "query" prop on button click', async () => {
+  const query = jest.fn()
+  render(<WeatherSearch query={query} />)
+  const locationInput = screen.getByPlaceholderText("Location")
+
+  userEvent.type(locationInput, "Maracay")
+  fireEvent.click(screen.getByText("boton"))
+
+  await waitFor(() =>
+    expect(query).toHaveBeenCalledWith(
+      { location: "Maracay" },
+
+      expect.anything()
+    )
+  )
+})
